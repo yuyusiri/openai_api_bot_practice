d4a36832e3dc5cc4c0b509645f27b49baaa85a6b
yuyusiri <144890515+yuyusiri@users.noreply.github.com>
2023-12-02 08:58:56 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if st.session_state["messages"]:
   messages = st.session_state["messages"]
   
   for message in reversed(messages[1:]): # 直近のメッセージを上に
-    st.write(message)
+    st.write(message["role"])
     speaker = "🙂" # 相談者
     # if message["role"]=="assistant":
     if ChatCompletionMessage["role"]=="assistant":
