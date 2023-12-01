525d8b23d85c9256e0f2607533c2c71006cf5c67
yuyusiri <144890515+yuyusiri@users.noreply.github.com>
2023-12-02 07:50:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def communicate():
     temperature=1.0 # ★★★
   )
 
-  bot_message = response["choices"][0]["message"]
+  # bot_message = response["choices"][0]["message"]
+  bot_message = response.choices[0].message
   messages.append(bot_message)
 
   st.session_state["user_input"] = "" #入力欄を消去
